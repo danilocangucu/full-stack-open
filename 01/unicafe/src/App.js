60faaf962a9aa65027f014980dd2a739d3b625db
danr0x <danilo@gritlab.ax>
2023-03-05 16:55:40 +0200

    unicafe copy of objects correction and refactoring

--- 01/unicafe/src/App.js
+++ 01/unicafe/src/App.js
@@ -4,120 +4,78 @@ const Header = () => <h1>give feedback</h1>
 
 const StatisticsHeader = () => <h1>statistics</h1>
 
-const Button = ({ handleClick, name }) => <button onClick={handleClick}>{name}</button>
+const Button = ({ handleClick, evaluation }) => <button onClick={handleClick}>{evaluation.name}</button>
 
-const StatisticLine = ({ text, value }) => {
-  if (text === 'positive'){
-    return (
-      <tr>
-        <td>{text}</td>
-        <td>{value} %</td>
-      </tr>
-    )
+const StatisticLine = ({ evaluation }) => {
+  if (evaluation.name === 'positive'){
+    return <tr><td>{evaluation.name}</td><td>{evaluation.value} %</td></tr>
   }
-  return (
-      <tr>
-        <td>{text}</td>
-        <td>{value}</td>
-      </tr>
-  )
+  return <tr><td>{evaluation.name}</td><td>{evaluation.value}</td></tr>
 }
 
-const Statistics = ({ allData }) => {
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
 
-  if (allData[3].value === 0){
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
+  if (all.value === 0){
+    return <div>No feedback given</div>
   }
 
   return (
       <table>
         <tbody>
-          <StatisticLine text={allData[0].name} value={allData[0].value} />
-          <StatisticLine text={allData[1].name} value={allData[1].value} />
-          <StatisticLine text={allData[2].name} value={allData[2].value} />
-          <StatisticLine text={allData[3].name} value={allData[3].value} />
-          <StatisticLine text={allData[4].name} value={allData[4].value} />
-          <StatisticLine text={allData[5].name} value={allData[5].value} />
+          <StatisticLine evaluation={good}/>
+          <StatisticLine evaluation={neutral}/>
+          <StatisticLine evaluation={bad}/>
+          <StatisticLine evaluation={all}/>
+          <StatisticLine evaluation={average}/>
+          <StatisticLine evaluation={positive}/>
         </tbody>
       </table>
   )
 }
 
-
-
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allEvaluations, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const positive = (good/(allEvaluations*100))*10000
-  const averageCalculation = average/allEvaluations
-
-  const allData = [{
-    name: "good",
-    value: good
-  }, {
-    name: "neutral",
-    value: neutral
-  },{
-    name: "bad",
-    value: bad
-  },{
-    name: "all",
-    value: allEvaluations
-  },{
-    name: "average",
-    value: averageCalculation
-  },{
-    name: "positive",
-    value: positive
-  }]
-
-  const allDataCopy = [...allData]
+  const [good, setGood] = useState({ name: "good", value: 0 })
+  const [neutral, setNeutral] = useState({ name: "neutral", value: 0 })
+  const [bad, setBad] = useState({ name: "bad", value: 0 })
+  const [allEvaluations, setAllEvaluations] = useState({ name: "all", value: 0 })
+  const [average, setAverage] = useState({ count: 0 })
+  const positive = { name: "positive", value: (good.value/(allEvaluations.value*100))*10000 }
+  const newAverage = { name: "average", value: (average.count)/(allEvaluations.value) }
 
   const handleGood = () => {
-    setGood(good + 1)
-    setAll(allEvaluations + 1)
-    setAverage(average + 1)
-    // allDataCopy[0] = {
-    //   ...allDataCopy[0],
-    //   value: good
-    // }
+    const newGood = { ...good, value: good.value + 1 }
+    const newAllEvaluations = { ...allEvaluations, value: allEvaluations.value + 1 }
+    const newAverage = { count: average.count + 1 }
+    setGood(newGood)
+    setAllEvaluations(newAllEvaluations)
+    setAverage(newAverage)
   }
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(allEvaluations + 1)
-    // allData[1] = {
-    //   ...allData[1],
-    //   value: neutral
-    // }
+    const newNeutral = { ...neutral, value: neutral.value + 1 }
+    const newAllEvaluations = { ...allEvaluations, value: allEvaluations.value + 1 }
+    setNeutral(newNeutral)
+    setAllEvaluations(newAllEvaluations)
   }
 
   const handleBad = () => {
-    setBad(bad + 1)
-    setAll(allEvaluations + 1)
-    setAverage(average - 1)
-    // allData[2] = {
-    //   ...allData[2],
-    //   value: bad
-    // }
+    const newBad = { ...bad, value: bad.value + 1 }
+    const newAllEvaluations = { ...allEvaluations, value: allEvaluations.value + 1 }
+    const newAverage = { count: average.count - 1 }
+    setBad(newBad)
+    setAllEvaluations(newAllEvaluations)
+    setAverage(newAverage)
   }
 
   return (
     <div>
       <Header />
-      <Button handleClick={handleGood} name="good"/>
-      <Button handleClick={handleNeutral} name="neutral"/>
-      <Button handleClick={handleBad} name="bad"/>
+      <Button handleClick={handleGood} evaluation={good}/>
+      <Button handleClick={handleNeutral} evaluation={neutral}/>
+      <Button handleClick={handleBad} evaluation={bad}/>
       <StatisticsHeader />
-      <Statistics allData={allData}/>
+      <Statistics good={good} neutral={neutral} bad={bad} all={allEvaluations} average={newAverage} positive={positive}/>
     </div>
   )
 }

df75ca828bf9cef1090110b195b06e9295b2a253
Danilo Costa Canguçu <daniloccangucu@gmail.com>
2023-10-03 10:59:50 +0200

    BlogForm.test.js updated and working with await

--- 05/5.13-5.16/frontend/src/components/BlogForm.test.js
+++ 05/5.13-5.16/frontend/src/components/BlogForm.test.js
@@ -1,13 +1,14 @@
 import React from "react";
-import '@testing-library/jest-dom'
-import { render, screen, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
 import BlogForm from "./BlogForm";
 import userEvent from '@testing-library/user-event'
 
 describe('<BlogForm />', () => {
-    const createBlog = jest.fn()
 
     test('form calls the event handler with right details', async () => {
+        const createBlog = jest.fn()
+
         render(<BlogForm createBlog={createBlog} />)
 
         const titleInput = screen.getByTestId('title')
@@ -15,18 +16,21 @@ describe('<BlogForm />', () => {
         const urlInput = screen.getByTestId('url')
         const saveButton = screen.getByText('save')
 
-        userEvent.type(titleInput, 'New blog')
-        userEvent.type(authorInput, 'New author')
-        userEvent.type(urlInput, 'http://new.url')
-
-        // screen.debug()
-
-        userEvent.click(saveButton)
-
-        expect(createBlog).toHaveBeenCalledWith({
+        const newBlog = {
             title: 'New blog',
             author: 'New author',
             url: 'New url'
-        })
+        }
+
+        await userEvent.type(titleInput, newBlog.title)
+        await userEvent.type(authorInput, newBlog.author)
+        await userEvent.type(urlInput, newBlog.url)
+
+        await userEvent.click(saveButton)
+
+        expect(createBlog.mock.calls).toHaveLength(1)
+        expect(createBlog.mock.calls[0][0].title).toBe(newBlog.title)
+        expect(createBlog.mock.calls[0][0].author).toBe(newBlog.author)
+        expect(createBlog.mock.calls[0][0].url).toBe(newBlog.url)
     })
 })

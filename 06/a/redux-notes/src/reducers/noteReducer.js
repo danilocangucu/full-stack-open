a761abe2e1d13ed78b9984daa577197aa7156d03
Danilo Costa Canguçu <daniloccangucu@gmail.com>
2023-10-11 19:33:08 +0300

    part 6 a complete

--- 06/a/redux-notes/src/reducers/noteReducer.js
+++ 06/a/redux-notes/src/reducers/noteReducer.js
@@ -1,5 +1,5 @@
 const noteReducer = (state = [], action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'NEW_NOTE':
             return [...state, action.payload]
         case 'TOGGLE_IMPORTANCE': {
@@ -9,12 +9,33 @@ const noteReducer = (state = [], action) => {
                 ...noteToChange,
                 important: !noteToChange.important
             }
-            return state.map(note => 
+            return state.map(note =>
                 note.id !== id ? note : changedNote
             )
         }
         default:
             return state
+    }
+}
+
+const generateId = () =>
+    Number((Math.random() * 1000000).toFixed(0))
+
+export const createNote = (content) => {
+    return {
+        type: 'NEW_NOTE',
+        payload: {
+            content,
+            important: false,
+            id: generateId()
+        }
+    }
+}
+
+export const toggleImportanceOf = (id) => {
+    return {
+        type: 'TOGGLE_IMPORTANCE',
+        payload: { id }
     }
 }
 
